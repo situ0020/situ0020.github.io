3b13b789935072ee6262bbd8d081175f57afa008
situ0020 <situ0020@algonquinlive.com>
2014-12-10 19:50:51 -0500

    Added fade to logo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ var $homeSection = $('#home')
 var $aboutSection = $('#about')
 var $portfolioSection = $('#portfolio')
 var $contactSection = $('#contact')
-
+var $logoSection = $('.home');
+var $logo = $('.main-logo');
+var $subhead = $('.subhead');
 
 $(".nav").localScroll();
 $(".contact").localScroll();
+
+
+$logoSection.waypoint(function () {
+	$logo.addClass('js-main-logo-fade');
+}, { offset: '50%' });
+
+$logoSection.waypoint(function () {
+	$subhead.addClass('js-subhead-fade');
+}, { offset: '50%' });
